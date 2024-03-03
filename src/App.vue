ba3339b625f23187e061bf35528c6a66a946bec7
rustamnew <1rustamnew1@gmail.com>
2024-03-04 00:39:11 +0300

    Реализация

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
 </script>
 
 <template>
+    <div class="page">
+        <RouterView />
+    </div>
 </template>
+
+<style>
+    #app > .page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        margin-bottom: 3rem;
+        font-size: 3rem;
+    }
+
+    div + .router-link {
+        margin-top: 2rem;
+    }
+</style>
